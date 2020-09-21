47f2f7cbf247bfe7593399b8f5df6fa62c50f142
unknown <khushalthepane2000@gmail.com>
2020-09-21 18:28:10 +0530

    errors

---
+++ backend/data.js
@@ -0,0 +1,26 @@
+const Data = [
+    {
+        url:"https://player.vimeo.com/external/418802080.sd.mp4?s=f54899a03a0e975cd3f3f811a9cae0829fdea229&profile_id=165&oauth2_token_id=57447761",
+        channel:"_khussshal_",
+        song:"Forever - Chris Brown",
+        likes: "1212",
+        messages:"3434",
+        description:"omea wa mo shinde iru",
+        shares:"2324",
+ },
+    {
+        
+        url:"https://www.facebook.com/vigovideo.official/videos/2971048579681019/",
+        channel:"Messi_10",
+        song:"Staying Alive - BG's",
+        likes: "1233",
+        messages:"3223",
+        description:"Goat on the ground",
+        shares:"2232",
+}
+];
+
+//bRg3LaLntCU0IWCD
+
+export default Data;
+
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,60 @@
-import express from 'express'
-import mongoos from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
+import Data from './data.js'
+import Videos from "./dbModel.js";
 
+//app config
 const app = express()
-const port = 9000;
+const port = process.env.PORT ||9000;
 
+//middlewares
+app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeaders('Access-Control-Allow-Origin','*'),
+    res.setHeaders('Access-Control-Allow-Headers','*'),
+    next()
+})
+
+//DB config
+const connection__url = 'mongodb+srv://khushalt10:bRg3LaLntCU0IWCD@cluster0.tz2dx.mongodb.net/tiktok?retryWrites=true&w=majority'
+
+mongoose.connect(connection__url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+})
+
+//api endpoints
 app.get('/', (req,res) => {
     res.status(200).send('Hello World!')
 })
 
+app.get('/v1/posts', (req,res) => {
+    res.status(200).send(Data)
+})
+app.get('/v2/posts', (req,res) => {
+    Videos.find((err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/v2/posts', (req,res) => {
+    const dbVideos = req.body;
+
+    Videos.create(dbVideos, (err, data) => {
+        if(err) {
+            res.status(500).send(err)
+        } else {
+            res.status(201).send(data)
+        }
+    })
+})
+
+
+
+//listen
 app.listen(port, () => console.log(`listening on localhost:${port}`))
